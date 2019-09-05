c7eccc481f433b5f8be13a0192b19f1b324a0782
indykiss <indirak@alumni.princeton.edu>
2019-09-04 21:29:58 -0400

    get demo ready

--- data-scape-client/src/components/Home.js
+++ data-scape-client/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home">
     <h1> Create a scape </h1>
     <h3> What's a scape? 
-        Input a stock, a start date and an end date, and you'll make a scape.
+      Input a stock, a start date and an end date, and you'll make a scape that tells you the stock ticker, company name and historical stock price from your selected start and end dates.
     </h3>
     </div>
   );
